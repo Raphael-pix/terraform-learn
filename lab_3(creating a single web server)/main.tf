8c5e4b33d468070cfc8ac048c56a7651a965f5ce
Raphael-pix <113975698+Raphael-pix@users.noreply.github.com>
2026-03-05 14:16:43 +0300

    Initial commit

---
+++ lab_3(creating a single web server)/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
